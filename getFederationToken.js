7ee1c7dacbdef3d2b15a24a049a48e7e8f885475
raluca-elena <ralucaelena1985@gmail.com>
2014-07-07 11:56:53 -0700

    added federation token request

---
+++ getFederationToken.js
@@ -0,0 +1,27 @@
+var aws = require('aws-sdk');
+aws.config.loadFromPath('config.json');
+var sts = new aws.STS();
+var policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+
+var params = {
+    Name: 'blue_unicorn', // required
+    DurationSeconds: 129600,
+    Policy: JSON.stringify(policy)
+};
+sts.getFederationToken(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+});
